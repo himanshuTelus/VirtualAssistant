e38013d01d8ff408c3c0b5848ed5b13ed7931467
Himanshu Goyal <himanshu.goyal@telusinternational.com>
2021-09-24 10:28:31 +0530

    Initial Commit

---
+++ VirtualAssistant/VirtualAssistant.h
@@ -0,0 +1,18 @@
+//
+//  VirtualAssistant.h
+//  VirtualAssistant
+//
+//  Created by xavient on 24/09/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VirtualAssistant.
+FOUNDATION_EXPORT double VirtualAssistantVersionNumber;
+
+//! Project version string for VirtualAssistant.
+FOUNDATION_EXPORT const unsigned char VirtualAssistantVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VirtualAssistant/PublicHeader.h>
+
+
